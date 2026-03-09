5e758c721cfaa2b2aa1b04a5c9ed02a5c0c0eb64
Andrew-Tinsley <Andrew.Tinsley@kellogg.northwestern.edu>
2026-03-08 19:47:55 -0500

    Implement login/logout in SessionsController

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(username: params["username"])
+    if @user && @user.authenticate(params["password"])
+      session[:user_id] = @user.id
+      redirect_to "/"
+    else
+      flash["notice"] = "Invalid username or password."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to "/login"
   end
 end
-  
